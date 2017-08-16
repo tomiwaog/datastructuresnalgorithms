c49e7414c6e9438587dab37409f9be2b9efe5fef
tomiwaog <tommiekiddy@hotmail.co.uk>
2017-08-16 13:38:18 +0100

    Main Method for testing other object and classes

--- src/datastructures/DataStructures.java
+++ src/datastructures/DataStructures.java
@@ -15,6 +15,20 @@ public class DataStructures {
         System.out.println(mystack.peek());
         System.out.print("\n--- Running Remove\n ---");
         System.out.println(mystack.pop());
+        
+        /*//Testing Person Stack
+        PersonStack newPStack = new PersonStack(2); //New Stack Bucket
+        Person person1 = new Person("Tomiwa", 28);
+        Person person2 = new Person("Adetona", 15);
+        newPStack.push(person1);
+        newPStack.push(person2);
+        System.out.println("After pushing");
+        newPStack.printStack();
+        System.out.println("\nNow popping");
+        newPStack.pop();
+        //newPStack.pop();
+        newPStack.printStack();
+        */
     }
 
 }
