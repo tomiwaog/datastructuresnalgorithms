85badb7b04f914172d37d3e0d286bf9e461cb6ff
Tomiwa Ogunbamowo <contact@tomiwa.io>
2018-12-23 22:06:44 +0000

    Menu for Testing implemented algos

--- src/algorithms/TestAlgo.java
+++ src/algorithms/TestAlgo.java
@@ -2,21 +2,23 @@ package algorithms;
 
 public class TestAlgo {
 public static void main(String[] args) {
-	QuickFindUnionFind x = new QuickFindUnionFind(10);
-	x.printer();
+	//QuickFind obj = new QuickFind(10);
+	QuickUnion obj = new QuickUnion(10);
+	obj.printer();
 	System.out.println("- - - - - - - - - -");
 	System.out.println();
-	x.union(4,3);x.printer();
-	x.union(3,8);x.printer();
-	x.union(6,5);x.printer();
-	x.union(9,4);x.printer();
-	x.union(2,1);x.printer();
-	x.isConnected(8, 9);
-	x.isConnected(5, 0);
-	x.union(5,0);x.printer();
-	x.union(7,2);x.printer();
-	x.union(6,1);x.printer();
+	obj.union(4,3);obj.printer();
+	obj.union(3,8);obj.printer();
+	obj.union(6,5);obj.printer();
+	obj.union(9,4);obj.printer();
+	obj.union(2,1);obj.printer();
+	obj.isConnected(8, 9);
+	obj.isConnected(5, 4);
+	obj.union(5,0);obj.printer();
+	obj.union(7,2);obj.printer();
+	obj.union(6,1);obj.printer();
+	obj.union(7,3);obj.printer();
 	
-	System.out.println("Component count is " + x.count());
+	System.out.println("Component count is " + obj.count());	
 }
 }
